dddd6fdea5d0adea3467d9d6fbb270d3420cf468
Rafał C <rafal.ciecwierz4@gmail.com>
2021-03-19 21:57:46 +0100

    First challenge done

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -4,10 +4,54 @@ console.log("This is react app");
 
 // JSX - JavaScript XML
 var template = React.createElement(
-  "p",
-  null,
-  "Does this change?"
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        "Does this change?"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "This is a paragraph"
+    ),
+    React.createElement(
+        "ol",
+        null,
+        React.createElement(
+            "li",
+            null,
+            "This is list item"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "This is list item"
+        )
+    )
 );
+
+var templateTwo = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        "Rafa\u0142 Cie\u0107wierz"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Age: 29"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Location: Pozna\u0144"
+    )
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -2,7 +2,27 @@ console.log("This is react app")
 
 
 // JSX - JavaScript XML
-var template = <p>Does this change?</p>
+var template = (
+    <div>
+        <h1>Does this change?</h1>
+        <p>This is a paragraph</p>
+        <ol>
+            <li>This is list item</li>
+            <li>This is list item</li>
+        </ol>
+    </div>
+);
+
+
+var templateTwo = (
+    <div>
+        <h1>Rafał Ciećwierz</h1>
+        <p>Age: 29</p>
+        <p>Location: Poznań</p>
+    </div>
+);
+
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
