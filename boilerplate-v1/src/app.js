9e9018ff8a2c986acfa8a2984ac69b600fcc4f7d
Rafał Ciećwierz <rafal.ciecwierz4@gmail.com>
2021-04-04 00:01:46 +0200

    ADD: INITIAL: Expensify and boilerplate setup

---
+++ boilerplate-v1/src/app.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// COMPONENTS
+
+
+
+// CSS STYLE
+import 'normalize.css/normalize.css'
+import './styles/styles.scss'
+
+ReactDOM.render(<p>⚛️ Welcome to React Boiler Plate ⚛️</p>, document.getElementById('app'));
---
+++ expensify-app/webpack.config.js
@@ -0,0 +1,36 @@
+// entry -> output
+const path = require('path');
+
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"]
+            },
+            {
+                test: /\.s?css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public')
+    }
+
+};
