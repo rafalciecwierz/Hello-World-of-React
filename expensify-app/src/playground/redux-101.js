23de76d1949070f8bf1a466c060fc22474b206eb
Rafał C <rafal.ciecwierz4@gmail.com>
2021-04-10 22:54:23 +0200

    EDIT: Add action functions

--- expensify-app/src/playground/redux-101.js
+++ expensify-app/src/playground/redux-101.js
@@ -1,17 +1,38 @@
 import { createStore } from 'redux';
 
+
+// Action generators = functions that return action objects
+const incrementCount = ( { incrementBy = 1 } = {} ) => ({
+        type: 'INCREMENT',
+        incrementBy
+});
+
+const decrementCount = ({ decrementBy = 1 } = {}) => ({
+    type: 'DECREMENT',
+    decrementBy
+});
+
+const setCount = ({ count = 0 } = {}) => ({
+    type: 'SET',
+    count
+});
+
+const resetCount =  () => ({
+    type: 'RESET'
+});
+
+
+
 const store = createStore((state = { count: 0 }, action) => {
 
     switch(action.type){
         case 'INCREMENT':
-            const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
             return {
-                count: state.count + incrementBy
+                count: state.count + action.incrementBy
             };
         case 'DECREMENT':
-            const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
             return {
-                count: state.count - decrementBy
+                count: state.count - action.decrementBy
             };
         case 'SET':
             return {
@@ -35,32 +56,24 @@ const unsubscribe = store.subscribe(() => {
 
 // Actions
 // increment, decrement, reset
-store.dispatch({
-    type: 'INCREMENT',
-    incrementBy: 5
-});
+// store.dispatch({
+//     type: 'INCREMENT',
+//     incrementBy: 5
+// });
 
-store.dispatch({
-    type: 'INCREMENT'
-});
+store.dispatch(incrementCount({ incrementBy: 5 }));
 
-store.dispatch({
-    type: 'RESET'
-});
+store.dispatch(incrementCount());
 
-store.dispatch({
-    type: 'DECREMENT',
-    decrementBy: 2
-});
+store.dispatch(resetCount());
 
-store.dispatch({
-    type: 'DECREMENT'
-});
+//unsubscribe();
 
-store.dispatch({
-    type: 'SET',
-    count: 101
-})
+store.dispatch(decrementCount({ decrementBy: 10 }));
+
+store.dispatch(decrementCount());
+
+store.dispatch(setCount({ count: 102 }))
 
 
 
--- expensify-app/webpack.config.js
+++ expensify-app/webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 
 module.exports = {
-    entry: './src/playground/descruturing.js',
+    entry: './src/playground/redux-101.js',
     output: {
         path: path.resolve(__dirname, 'public'),
         filename: 'bundle.js'
