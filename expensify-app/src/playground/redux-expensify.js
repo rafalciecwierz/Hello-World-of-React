a09bdf6caaa3a059aa30d57a71c0a482db1e8ab7
Rafał C <rafal.ciecwierz4@gmail.com>
2021-04-13 23:50:26 +0200

    ADD: Array and Object spread operator

--- expensify-app/src/playground/redux-expensify.js
+++ expensify-app/src/playground/redux-expensify.js
@@ -1,8 +1,29 @@
 import { createStore, combineReducers } from 'redux';
+import uuid from 'uuid';
 
 
 // ADD_EXPENSE
+const addExpense = (
+    {
+        description = '',
+        note = '',
+        amount = 0,
+        createdAt = 0
+    } = {} ) => ({
+    type: 'ADD_EXPENSE',
+    expense: {
+        id: uuid(),
+        description,
+        note,
+        amount,
+        createdAt
+    }
+});
 // REMOVE_EXPENSE
+const removeExpense = ({id} = {}) => ({
+    type: 'REMOVE_EXPENSE',
+    id
+});
 // EDIT_EXPENSE
 // SET_TExT_FILTER
 // SORT_BY_DATE
@@ -15,10 +36,15 @@ import { createStore, combineReducers } from 'redux';
 const expensesReducerDefaultState = []
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
     switch (action.type) {
+        case 'ADD_EXPENSE':
+            return [...state, action.expense]
+        case 'REMOVE_EXPENSE':
+            return state.filter(({id}) => id !== action.id)
         default:
             return state;
     }
 };
+
 
 // Filters Reducer
 const filtersReducerDefaultState = {
@@ -35,15 +61,37 @@ const filersReducer = (state = filtersReducerDefaultState, action) => {
 };
 
 
+
 // Store creation
 const store = createStore(
     combineReducers({
         expenses: expensesReducer,
         filters: filersReducer
     })
-    );
+);
 
-console.log(store.getState());
+
+store.subscribe(() => {
+    console.log(store.getState());
+});
+
+
+
+const expenseOne = store.dispatch(addExpense({
+    description: 'Rent',
+    amount: 100
+}));
+
+const expenseTwo = store.dispatch(addExpense({
+    description: 'Coffee',
+    amount: 300,
+    note: 'It was delicious'
+}));
+
+console.log(expenseOne.expense.id);
+store.dispatch(removeExpense({ id: expenseOne.expense.id }))
+
+
 
 
 const demoState = {
@@ -61,3 +109,4 @@ const demoState = {
          endDate: undefined
      }
 }
+
