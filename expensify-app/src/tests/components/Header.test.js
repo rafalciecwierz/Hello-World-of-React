92d6497fe8da1fc799936b810b735449a4d73a8e
Rafał Ciećwierz <rafal.ciecwierz4@gmail.com>
2022-01-08 18:53:32 +0100

    Add: setting up Enzyme

--- expensify-app/src/tests/components/Header.test.js
+++ expensify-app/src/tests/components/Header.test.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import ReactShallowRenderer  from 'react-test-renderer/shallow';
 import Header from '../../components/Header';
+import { shallow } from 'enzyme';
 
 test('should render header correctly', () => {
-    const renderer = new ReactShallowRenderer();
-    renderer.render(<Header/> );
-    console.log(renderer.getRenderOutput());
-    expect(renderer.getRenderOutput()).toMatchSnapshot();
+    const wrapper = shallow(<Header/>);
+
+    expect(wrapper).toMatchSnapshot();
+    // expect(wrapper.find('h1').text()).toBe('Expensify');
+    // const renderer = new ReactShallowRenderer();
+    // renderer.render(<Header/> );
+    // expect(renderer.getRenderOutput()).toMatchSnapshot();
 })
